4fb3dc9aeb3869ef0bd326774181f9c712e7c8a5
汪洋 <yang.wang01@luckincoffee.com>
2021-06-10 15:13:33 +0800

    add ipstringcountbetweentwoip

--- net/ip2number.go
+++ net/ip2number.go
@@ -22,3 +22,20 @@ func IPCountBetweenTwoIP(ip1, ip2 net.IP) (uint, error) {
 	}
 	return num2 - num1, nil
 }
+
+func IPString2Number(ip string) (uint, error) {
+	b := net.ParseIP(ip).To4()
+	if b == nil {
+		return 0, errors.New("invalid ipv4 format")
+	}
+	return uint(b[3]) | uint(b[2])<<8 | uint(b[1])<<16 | uint(b[0])<<24, nil
+}
+
+func IPCountBetweenTwoIPString(ip1, ip2 string) (uint, error) {
+	num1, err := IPString2Number(ip2)
+	num2, err := IPString2Number(ip1)
+	if err != nil {
+		return 0, errors.Wrap(err, "Get count between two ip")
+	}
+	return num2 - num1, nil
+}
